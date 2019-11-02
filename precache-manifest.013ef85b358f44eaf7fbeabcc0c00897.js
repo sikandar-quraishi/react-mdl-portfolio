09072ca09241b6d9b21694aa1ffd127ade8732c7
zuaam <nriittech@gmail.com>
2019-11-02 06:20:10 -0700

    Updates

---
+++ precache-manifest.013ef85b358f44eaf7fbeabcc0c00897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b4fd6fc27be415dc1aa4afaab712ce",
+    "url": "sikandar-quraishi.github.io/react-mdl-portfolio/index.html"
+  },
+  {
+    "revision": "0bc0601e55c71c60def1",
+    "url": "sikandar-quraishi.github.io/react-mdl-portfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "e2df7dbef005a7698c87",
+    "url": "sikandar-quraishi.github.io/react-mdl-portfolio/static/css/main.7dba8795.chunk.css"
+  },
+  {
+    "revision": "0bc0601e55c71c60def1",
+    "url": "sikandar-quraishi.github.io/react-mdl-portfolio/static/js/2.c5ef8b52.chunk.js"
+  },
+  {
+    "revision": "e2df7dbef005a7698c87",
+    "url": "sikandar-quraishi.github.io/react-mdl-portfolio/static/js/main.f8d4df06.chunk.js"
+  },
+  {
+    "revision": "2670617b0bf4812bbaa5",
+    "url": "sikandar-quraishi.github.io/react-mdl-portfolio/static/js/runtime-main.ea5260ea.js"
+  }
+]);
